53a8a02f24ddaf00edb8115afbcc09bf831c97f2
Ricardo Barbosa <ric3639@gmail.com>
2020-02-19 12:47:49 -0800

    index.js

--- server.js
+++ server.js
@@ -3,11 +3,33 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+/**
+ * logger logs to the console the following information 
+ * about each request:
+ * request method
+ * request url
+ * and a timestamp
+ * 
+ * This mw runs on every request made to the API
+ */
 
+ //create a mw function for requestTime
+//  var requestTime = function(req, res, next) {
+//    req.requestTime = Date.now()
+//    next()
+//  }
+
+function logger(req, res, next) {}
+  const { method, url, requestTime } = req
+  const agent = req.get("User-Agent")
+  req.requestTime = Date.now()
+
+  console.log(` ${method} ${url} ${requestTime} ${agent}`)
+
+  
 module.exports = server;
